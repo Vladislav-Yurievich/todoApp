28a1b7fb90ab696277a5a0eb9ac58dde8703dc22
VladislavBiryukov <58136116+Vladislav-Yurievich@users.noreply.github.com>
2025-03-18 21:39:32 +0500

    Update Auth.jsx

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -56,7 +56,7 @@ const Auth = () => {
 		<div className='flex min-h-full flex-col pt-20 px-6 py-12 lg:px-8'>
 			<div className='sm:mx-auto sm:w-full sm:max-w-sm'>
 				<h2 className='mt-10 text-center text-3xl/9 font-bold tracking-tight'>
-					
+					{isLogIn ? 'Авторизация' : 'Регистрация'}
 				</h2>
 			</div>
 
