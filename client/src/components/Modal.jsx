265b9671e88e7954716cd443d63fabfd7e0dfcb3
VladislavBiryukov <accountformywork@mail.ru>
2025-03-22 23:14:30 +0500

    wip

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useCookies } from 'react-cookie'
 import { X } from 'lucide-react'
 import useFetchUsers from '../hooks/useFetchUsers'
@@ -86,25 +86,6 @@ const Modal = ({ mode, setShowModal, getData, task }) => {
 
 								<div className='col-span-2'>
 									<label
-										htmlFor='description'
-										className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-									>
-										Описание задачи
-									</label>
-									<textarea
-										name='description'
-										value={data.description}
-										className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-										placeholder='Введите описание задачи'
-										id='description'
-										rows='2'
-										required
-										onChange={handleChange}
-									></textarea>
-								</div>
-
-								<div className='col-span-2'>
-									<label
 										htmlFor='dueDate'
 										className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
 									>
@@ -175,6 +156,26 @@ const Modal = ({ mode, setShowModal, getData, task }) => {
 								</div>
 							</>
 						)}
+
+						<div className='col-span-2'>
+							<label
+								htmlFor='description'
+								className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+							>
+								Описание задачи
+							</label>
+							<textarea
+								name='description'
+								value={data.description}
+								className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+								placeholder='Введите описание задачи'
+								id='description'
+								rows='2'
+								required
+								readOnly={userRole !== 'MANAGER'}
+								onChange={handleChange}
+							></textarea>
+						</div>
 						<div className='col-span-2'>
 							<label
 								htmlFor='status'
